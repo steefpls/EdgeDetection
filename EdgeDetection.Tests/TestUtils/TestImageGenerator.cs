8a4049fa96293b7547a6abe152c545ff99262c13
steefpls <steven.koe80@gmail.com>
2025-02-08 03:44:52 +0800

    Implemented: TestImageGenerator util functions
    Generate grayscale gradient image, as well as uniform image.

--- EdgeDetection.Tests/TestUtils/TestImageGenerator.cs
+++ EdgeDetection.Tests/TestUtils/TestImageGenerator.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EdgeDetection.Core.Models;
 
 namespace EdgeDetection.Tests.TestUtils
 {
-    internal class TestImageGenerator
+    public static class TestImageGenerator
     {
+        public static GrayscaleImage CreateTestImage(int width, int height)
+        {
+            var image = new GrayscaleImage(width, height);
+
+            // Create a simple gradient pattern
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    byte value = (byte)(((x + y) % 256));
+                    image.SetPixel(x, y, value);
+                }
+            }
+
+            return image;
+        }
+
+        public static GrayscaleImage CreateUniformImage(int width, int height, byte value)
+        {
+            var image = new GrayscaleImage(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    image.SetPixel(x, y, value);
+                }
+            }
+
+            return image;
+        }
     }
 }
